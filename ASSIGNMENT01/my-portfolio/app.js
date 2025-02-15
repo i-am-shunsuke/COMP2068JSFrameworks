bc685c4e291f693b831f4ab92914cf65c30c5f47
i-am-shunsuke <200557414@student.georgianc.on.ca>
2025-02-15 17:12:37 -0500

    Edit

--- ASSIGNMENT01/my-portfolio/app.js
+++ ASSIGNMENT01/my-portfolio/app.js
@@ -20,8 +20,8 @@ hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 //
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
